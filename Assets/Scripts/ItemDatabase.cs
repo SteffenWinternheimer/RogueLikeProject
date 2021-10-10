911fe8350209d706469569cf427b63f67e93ff89
Steffen Winternheimer <steffen.winternheimer@gmail.com>
2021-10-10 14:25:46 +0200

    Instantiate project

---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour {
+    public List<Item> items = new List<Item>();
+
+    void Awake()
+    {
+        BuildDatabase();
+    }
+
+    public Item GetItem(int id)
+    {
+        return items.Find(item=> item.id == id);
+    }
+
+    public Item GetItem(string itemName)
+    {
+        return items.Find(item => item.title == itemName);
+    }
+
+    void BuildDatabase()
+    {
+        items = new List<Item>() {
+            new Item(0, "Banana", "A delicious Banana.",
+            new Dictionary<string, int> {
+                { "Carb", 15 },
+                { "Health", 10 }
+            }),
+            new Item(1, "Bean", "A small little bean.",
+            new Dictionary<string, int> {
+                { "Health", 3 }
+            }),
+            new Item(2, "Cherry", "A sweet cherry.",
+            new Dictionary<string, int> {
+                { "Carb", 5 },
+                { "Health", 20}
+            }),
+            new Item(3, "Radish", "A radish. Core ingredient for making food.",
+            new Dictionary<string, int> {
+                { "Carb", 5 },
+                { "Health", 20}
+            })
+        };
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody2D rigidbody2D;
+    public Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MoveCharacter(movement);
+    }
+
+    public void MoveCharacter(Vector2 direction)
+    {
+        rigidbody2D.MovePosition((Vector2)transform.position + (direction * movementSpeed * Time.deltaTime));
+    }
+}
